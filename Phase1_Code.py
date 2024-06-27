f2387551d757b139011127868fd80aa0efc7e3ef
dannyjf93 <dannygfavuzza@gmail.com>
2024-06-27 15:43:24 -0400

    phase 1 script

--- Phase1_Code.py
+++ Phase1_Code.py
@@ -19,21 +19,14 @@ req = requests.get(URL)
 req.status_code
 
 soup = bs4.BeautifulSoup(req.text, 'html.parser')
+table = soup.find('table', class_='table-striped')
+headers = table.findAll('th')
+product_data = table.findAll('td')
 
-table = soup.findAll('table')[0]
-rows = table.findAll('tr')
+#how do I get the actual text from this portion of the product page if the paragraph is separated from the ID/Class?
+product_description = soup.findAll('p', class_='sub-header')
 
-for row in rows[0:]:
-    cells = row.findAll('th')
-    UPC = cells[0].text.strip()
-    Product_Type = cells[1].text.strip()
-    Price_Excluding_Tax = cells[2].text.strip()
-    Price_Including_Tax = cells[3].text.strip()
-    Tax = cells[4].text.strip()
-    Availability = cells[5].text.strip()
-    Number_of_Reviews = cells[6].text.strip()
-    print(UPC, Product_Type, Price_Excluding_Tax, Price_Including_Tax, Tax, Availability, Number_of_Reviews)
-    print()
+print(headers, product_data)
 
 HEADERS = [
     'product_page_url',
