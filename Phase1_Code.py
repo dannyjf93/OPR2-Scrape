f596a3d643042bc7f07e4d2f983b70361e80fd4d
dannyjf93 <dannygfavuzza@gmail.com>
2024-06-27 14:26:39 -0400

    phase 1 script

--- Phase1_Code.py
+++ Phase1_Code.py
@@ -24,6 +24,26 @@ table = soup.findAll('table')[0]
 rows = table.findAll('tr')
 
 for row in rows[0:]:
-    cells = row.findAll('td')
+    cells = row.findAll('th')
     UPC = cells[0].text.strip()
-    print(UPC)
+    Product_Type = cells[1].text.strip()
+    Price_Excluding_Tax = cells[2].text.strip()
+    Price_Including_Tax = cells[3].text.strip()
+    Tax = cells[4].text.strip()
+    Availability = cells[5].text.strip()
+    Number_of_Reviews = cells[6].text.strip()
+    print(UPC, Product_Type, Price_Excluding_Tax, Price_Including_Tax, Tax, Availability, Number_of_Reviews)
+    print()
+
+HEADERS = [
+    'product_page_url',
+    'universal_product_code',
+    'book_title',
+    'price_including_tax',
+    'price_excluding_tax',
+    'quantity_available',
+    'product description',
+    'category',
+    'review_rating',
+    'image_url'
+]
