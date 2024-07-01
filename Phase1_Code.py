2e126c8b99cf49779b6b463b0a11105ff3329c28
dannyjf93 <dannygfavuzza@gmail.com>
2024-07-01 16:31:21 -0400

    phase 1 script

--- Phase1_Code.py
+++ Phase1_Code.py
@@ -28,6 +28,16 @@ bs_descriptions = soup.findAll('p')
 description = bs_descriptions[3].string
 header = headers[0:6]
 product_data = product_info[0:6]
+bs_titles = soup.findAll('h1')
+title = bs_titles[0].string
+bs_categories = soup.find_all('a')
+category = bs_categories[3].string
+review_rating = bs_descriptions[2].string
+image_url = soup.findAll('img')
 
 print(bs_descriptions[3].string)
 print(header[0].string, product_data[0].string)
+print(title)
+print(category)
+print(review_rating)
+print(image_url)
